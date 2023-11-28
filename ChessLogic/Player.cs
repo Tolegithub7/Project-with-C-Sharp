cf79bd8f1881fafe2e60d4737cb45696f6861286
HP <HP@TOLE-008HNA1>
2023-11-28 22:45:17 +0300

    Add project files.

---
+++ ChessLogic/Player.cs
@@ -0,0 +1,21 @@
+﻿namespace ChessLogic
+{
+    public enum Player
+    {
+        None,
+        White,
+        Black
+    }
+    public static class PlayerExtensions
+    {
+        public static Player Opponent(this Player player)
+        {
+            return player switch
+            {
+                Player.Black => Player.Black,
+                Player.White => Player.White,
+                _ => Player.None,
+            };
+        }
+    }
+}
